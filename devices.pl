8ba5d5fecb01188aa1fa55b60c2851ac0cd71749
pasha <pashamozg@gmail.com>
2012-03-10 10:48:10 +0700

    First commit

---
+++ devices.pl
@@ -0,0 +1,175 @@
+#!/usr/bin/perl
+
+use strict;
+
+use Net::SNMP;
+use Net::IP;
+use Storable;
+use Data::Dumper;
+use Time::HiRes qw(tv_interval gettimeofday);
+my $d_store_path = 'dev_store';
+my @ip_ranges = ('192.168.2.0/24', '172.31.31.0/24');
+my $community = 'read';
+my @oids = (
+        '1.3.6.1.2.1.31.1.1.1.6',
+        '1.3.6.1.2.1.31.1.1.1.10',
+        '1.3.6.1.2.1.2.2.1.14',
+        '1.3.6.1.2.1.2.2.1.20',
+        '1.3.6.1.2.1.2.2.1.5',
+        '1.3.6.1.2.1.2.2.1.2',
+);
+my %oids_ps = (
+    '1.3.6.1.2.1.31.1.1.1.6' => 1, 
+    '1.3.6.1.2.1.31.1.1.1.10' => 1,
+    );
+my %aux_oids = (
+    'ifspeed' => '1.3.6.1.2.1.2.2.1.5',
+    'ifnum' => '1.3.6.1.2.1.2.1.0',
+    'descr' => '1.3.6.1.2.1.2.2.1.2',
+);
+my $devices;
+my %skip_ip;
+if(-f $d_store_path){
+    my $t0 = [gettimeofday];
+    $devices =  retrieve($d_store_path);
+    print "load time : ", tv_interval ( $t0, [gettimeofday]),"\n";
+
+}
+
+sub get_callback
+{
+    my ($session, $shared, $type, $oids) = @_;
+    my $result = $session->var_bind_list();
+    my $t = time;
+    if (!defined $result) {
+#        printf "ERROR: Get request failed for host '%s': %s.\n",
+#              $session->hostname(), $session->error();
+        return -1;
+    }
+    printf "callback from host '%s' .\n",
+           $session->hostname();
+    if($type == 1){
+        my $t = time;
+        foreach(keys %$result){
+            if($_ =~ /^($aux_oids{descr}|$aux_oids{ifspeed}).\d{1,2}$/){
+                $shared->{$_}= $result->{$_}||'';
+            }else{
+                $shared->{$_}->{$t} = $result->{$_};
+            }
+#        print "-$_-", $result->{$_},"-\n";
+        }
+    }else{
+        if($result->{$aux_oids{ifnum}} > 0){
+            $shared->{$session->hostname()}->{num} = $result->{$aux_oids{ifnum}};
+        }
+    }
+
+    return;
+}
+sub snmp_req {
+    my ($ip , $oids , $shared, $type) = @_;
+    my ($session, $error) = Net::SNMP->session(
+            -hostname     =>  $ip,
+            -community    =>  $community,
+            -version    => 'snmpv2c',
+            -timeout    => 2,
+            -retries    => 2,
+            -nonblocking   => 1,
+            );  
+
+    if (!defined $session) {
+        printf "ERROR -$ip-  exit: %s.\n", $error;
+        return -1;
+    }   
+
+    my $result = $session->get_request(
+            -varbindlist => $oids,
+            -callback  => [ \&get_callback , $shared, $type, $oids],
+            );  
+
+    if (!defined $result) {
+        printf "ERROR: %s.\n", $session->error();
+        $session->close();
+        return -1;
+    } 
+}
+sub search_new_devices {
+    my ($devices) = @_;
+    foreach (@ip_ranges){
+        my $ip = new Net::IP ("$_") or die (Net::IP::Error());
+        my %dp;
+        my $host_count;
+        do{
+            if(exists $devices->{$ip->ip()}){
+#                next;
+            }
+            if( snmp_req( $ip->ip(), [$aux_oids{ifnum}], $devices) == -1 ){
+                next;
+            }
+            ++$host_count;
+            if($host_count==150){
+                snmp_dispatcher();
+                $host_count=0;
+            }
+        }while(++$ip);
+        snmp_dispatcher();
+    }
+    return $devices;
+}
+
+sub polling_devices {
+    my ($devices) = @_;
+    print "polling \n";
+    foreach my $ip (keys %$devices){
+        foreach my $int (1 .. $devices->{$ip}->{num}){
+            my @r_oids = map {$_.".$int"} @oids;
+            snmp_req($ip, \@r_oids, $devices->{$ip}->{int}, 1);
+        }
+        snmp_dispatcher();
+    }
+# tranform data to use in rrdtool
+# calculate speed on interface
+# set description 
+# set errors
+    foreach my $ip (keys %$devices){
+        foreach my $p (keys %{$devices->{$ip}->{int}}){
+            next if ($p =~ /^$aux_oids{ifspeed}/);
+            $p =~ /^((\d{1,2}\.?)+)\.(\d+)$/;
+            my ($oid , $int) = ($1, $3);
+            if($oid eq "$aux_oids{descr}" && $devices->{$ip}->{int}->{$p} ne ''){
+                $devices->{$ip}->{stat}->{$int}->{name} =  $devices->{$ip}->{int}->{$p};
+            }else{
+                my @t = sort keys %{$devices->{$ip}->{int}->{$p}};
+                if($#t>0){
+                    if($devices->{$ip}->{int}->{$p}->{$t[$#t]} =~ /noSuch/){
+                        delete $devices->{$ip}->{int}->{$p}; 
+                        delete $devices->{$ip}->{stat}->{$int}->{$oid};
+                    }
+                    if($oids_ps{$oid}){
+                        $devices->{$ip}->{stat}->{$int}->{$oid}->{$t[$#t]} = 
+                            $devices->{$ip}->{int}->{$p}->{$t[$#t]} - $devices->{$ip}->{int}->{$p}->{$t[$#t-1]};
+                        my $val =  int( $devices->{$ip}->{stat}->{$int}->{$oid}->{$t[$#t]}/ ($t[$#t] - $t[$#t-1])) ;
+                        if( $val*8 > $devices->{$ip}->{int}->{$aux_oids{ifspeed}.".$int"}){
+                            $val = ($devices->{$ip}->{int}->{$aux_oids{ifspeed}.".$int"})/8;
+                        }
+                        $devices->{$ip}->{stat}->{$int}->{$oid}->{$t[$#t]}  = $val;
+                        print $devices->{$ip}->{stat}->{$int}->{$oid}->{$t[$#t]} ,"\n";
+                    }else{
+                        $devices->{$ip}->{stat}->{$int}->{$oid}->{$t[$#t]} =
+                            $devices->{$ip}->{int}->{$p}->{$t[$#t]};
+                    }
+                    foreach(0 .. ($#t-2)){ 
+                        delete $devices->{$ip}->{int}->{$p}->{$t[$_]};
+                    }
+                }
+            }
+        }
+    }
+    return $devices;
+
+}
+
+$devices = search_new_devices($devices);
+$devices = polling_devices($devices);
+#print Dumper($devices);
+store $devices , $d_store_path;
---
+++ port_mon.pl
@@ -0,0 +1,140 @@
+#!/usr/bin/perl
+use Storable;
+use CGI qw/:standart/;
+use File::Path;
+use RRDTool::OO;
+use Data::Dumper;
+my @kv = (
+       {'1.3.6.1.2.1.31.1.1.1.6' =>'bytesIn',
+        '1.3.6.1.2.1.31.1.1.1.10' =>'bytesOut',
+       },
+       {'1.3.6.1.2.1.2.2.1.14'=>'errorIn',
+        '1.3.6.1.2.1.2.2.1.20'=>'errorOut',
+        },
+    );
+my $d_store_path = '/home/pasha/projects/port_mon/dev_store';
+my $image_refresh_time = 60;
+my $q = CGI->new();
+
+my $dev = $q->param('device');
+my $port = $q->param('port');
+
+my $devices;
+
+if(-e $d_store_path){
+    $devices =  retrieve($d_store_path);
+}
+
+sub rrd_graph {
+    my ($rrd, $file, $start, $end, $ds) = @_;
+    my @colors = qw/FF0000 00FF00 0000FF/;
+    my @draw;
+    foreach(0..$#{$ds}){
+        my %tmp = (
+            draw           => { 
+                dsname    => $ds->[$_],
+                thickness => 1,
+                color     => $colors[$_],
+                legend    => $ds->[$_],
+                cfunc   => 'AVERAGE',
+                },
+                );
+        push @draw, %tmp;
+    }
+#print Dumper($ds, \@draw);
+    if( (stat($file))[9] < $end-$image_refresh_time){
+        $rrd->graph(
+                image          => $file,
+                vertical_label => 'bytes per second',
+                start          => $start,
+                end            => $end ,
+                @draw,
+        );
+    }
+}
+print $q->header,
+      $q->start_html('Page with device statistics');
+if(defined $dev && defined $port){
+    my $path = '../htdocs/img';
+    my $end = time();
+    my $start  = $end - (300*12*24*31);
+    foreach my $in_out (@kv){
+    my $rrd = RRDTool::OO->new( file => "$path/myrrdfile.rrd" );
+    my @ds = values %$in_out;
+    my @oids = keys %$in_out;
+    my $base_name = ($ds[0]=~/bytes/)?'bytes':'errors';
+    $rrd->create(
+            step        => 60,  # one-second intervals
+            start       => $start,
+            data_source => { 
+            name      => $ds[0],
+            type      => "GAUGE" },
+            data_source => {
+            name    => $ds[1],
+            type    => 'GAUGE'},
+            archive     => { 
+            rows      => 60*24*31,
+#cpoints   => 5,
+                cfunc     => 'AVERAGE',
+            },
+            archive => {
+            rows    => 60*24*31,
+                cfunc     => 'AVERAGE',
+            },
+            );
+    if(exists $devices->{$dev}->{stat}->{$port}){
+        my @t = sort keys %{$devices->{$dev}->{stat}->{$port}->{$oids[0]}};
+            foreach my $t ( @t){
+              unless($t > $start && $t < $end ){ next ;}
+                    my $vals;
+                foreach my $oid ( 0..$#oids  ){
+                    $vals->{$ds[$oid]} =  $devices->{$dev}->{stat}->{$port}->{$oids[$oid]}->{$t} ;
+                }
+#                    print $t , " ", values %$vals, " ",keys %$vals,"\n";
+                    $rrd->update(time => $t , values => $vals );
+            }
+            unless(-d "$path/$dev/$port"){
+                mkpath "$path/$dev/$port";
+            }
+            rrd_graph($rrd, "$path/$dev/$port/${base_name}_4h.png", $end-3600, $end , \@ds);
+            rrd_graph($rrd, "$path/$dev/$port/${base_name}_12h.png", $end-12*3600, $end ,\@ds);
+            rrd_graph($rrd, "$path/$dev/$port/${base_name}_24h.png", $end-24*3600, $end ,\@ds);
+            rrd_graph($rrd, "$path/$dev/$port/${base_name}_w.png", $end-7*24*3600, $end ,\@ds);
+        }
+    }
+            foreach( keys %$devices){
+                print "<a href=port_mon.pl?device=$_> $_ </a> &nbsp&nbsp";
+            }
+            print $q->h1("Statistics for port ".$devices->{$dev}->{stat}->{$port}->{name}." on device $dev ");
+            print "<table>";
+            foreach my $p (qw/4h 12h 24h w/){
+                print "<tr>";
+                foreach $be (qw/bytes errors/){
+                    print "<td><image src = \"../img/$dev/$port/${be}_$p.png\" > </image></td>";
+                }
+                print "</tr>\n";
+            }
+            print "</table>\n";
+    }elsif(defined $dev){
+        print $q->h1("Knowing ports for $dev");
+        print "<table>\n";
+        if(exists $devices->{$dev}){
+            foreach(sort {$a <=> $b} keys %{$devices->{$dev}->{stat}}){
+                print "<tr><td>";
+                print "<a href=port_mon.pl?device=$dev&port=$_> $dev / $_ (".$devices->{$dev}->{stat}->{$_}->{name}.") </a>";
+                print "</td></tr>\n";
+            }
+            print "</table>";
+        }
+    }else{
+        print $q->h1('Knowing devices');
+        print "<table>\n";
+        foreach(keys %$devices){
+            print "<tr><td>";
+            print "<a href=port_mon.pl?device=$_> $_ </a>";
+            print "</td></tr>\n";
+        }
+        print "</table>";
+    }
+
+    print $q->end_html;
